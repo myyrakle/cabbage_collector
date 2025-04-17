7f9cc00837511a434b5d822366f2fbc2cd44d5f3
myyrakle <sssang97@naver.com>
2025-04-18 02:39:23 +0900

    [#1] gc 전역 객체 추가

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
+use std::sync::LazyLock;
+
 pub struct ObjectPool {}
 
 pub trait CabbageObject {
     fn get_child_objects(&self) -> Vec<*mut dyn CabbageObject>;
+
+    fn mark(&mut self, marked: bool);
+    fn is_marked(&self) -> bool;
+    fn free(&mut self);
 }
 
 pub struct CabbageCollector {
     roots: Vec<*mut dyn CabbageObject>,
 }
+
+pub static mut GC: LazyLock<CabbageCollector> =
+    LazyLock::new(|| CabbageCollector { roots: Vec::new() });
