c13c4a566ab1f5f4f297a6dbb2c9c2905af71968
myyrakle <sssang97@naver.com>
2025-04-20 01:06:28 +0900

    [#2] CabbageBox Debug 표현 개선

--- src/cabbage_box.rs
+++ src/cabbage_box.rs
@@ -1,8 +1,9 @@
+use core::fmt;
 use std::ops::{Deref, DerefMut};
 
 use crate::{COLLECTOR, raw::RawCabbage};
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct CabbageBox<T> {
     raw_cabbage: RawCabbage,
     _type: std::marker::PhantomData<T>,
@@ -26,6 +27,12 @@ impl<T> CabbageBox<T> {
 
     fn get_data_mut(&mut self) -> &mut T {
         self.raw_cabbage.get_data_mut()
+    }
+}
+
+impl<T: std::fmt::Debug> std::fmt::Debug for CabbageBox<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fmt::Debug::fmt(self.get_data_ref(), f)
     }
 }
 
